894f25a4d5450dbe6055c5e4b4ff947e16787318
Matteo Rossi <teo.red90@gmail.com>
2019-12-08 23:24:18 +0200

    Minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyexpokit',
+    version='0.0.1',
+    url='https://github.com/matteoacrossi/pyexpokit.git',
+    author='Matteo Rossi',
+    description="Python implementation of Expokit's expv",
+    packages=find_packages(),    
+    install_requires=['scipy'],
+)
