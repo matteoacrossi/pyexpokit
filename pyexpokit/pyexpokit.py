c66884ab35a53f6195a14bfb4c30ba07dabfeae9
Matteo Rossi <teo.red90@gmail.com>
2019-12-10 00:50:12 +0200

    PEP8 check

--- pyexpokit/pyexpokit.py
+++ pyexpokit/pyexpokit.py
@@ -3,9 +3,9 @@ A Python implementation of expm from Expokit
 """
 
 import numpy as np
-from scipy.sparse.linalg import norm as spnorm
-
 from numpy.linalg import norm
+
+from scipy.sparse.linalg import norm as spnorm
 from scipy.linalg import expm
 
 def expmv(t, A, v, tol=1e-7, krylov_dim=30):
@@ -14,7 +14,7 @@ def expmv(t, A, v, tol=1e-7, krylov_dim=30):
         techniques and matrix-vector product operations.
 
     This function implements the expv function of the Expokit library
-    (https://www.maths.uq.edu.au/expokit). It is in particular suitable for 
+    (https://www.maths.uq.edu.au/expokit). It is in particular suitable for
     large sparse matrices.
 
     Args:
@@ -22,7 +22,7 @@ def expmv(t, A, v, tol=1e-7, krylov_dim=30):
     A (array or sparse): an numpy.array or scipy.sparse square matrix
     v (array): a vector with size compatible with A
     tol (real): the required tolerance (default 1e-7)
-    krylov_dim (int): dimension of the Krylov subspace 
+    krylov_dim (int): dimension of the Krylov subspace
                       (typically a number between 15 and 50, default 30)
 
     Returns:
@@ -56,12 +56,12 @@ def expmv(t, A, v, tol=1e-7, krylov_dim=30):
     outvec = np.zeros(v.shape, dtype=out_type)
 
     # storage for Krylov subspace vectors
-    vm = np.zeros((m + 1, len(v)), dtype=out_type)
+    v_m = np.zeros((m + 1, len(v)), dtype=out_type)
     # for i in range(1, m + 2):
     #     vm.append(np.empty_like(outvec))
-    hm = np.zeros((m+2, m+2), dtype=outvec.dtype)
+    h_m = np.zeros((m+2, m+2), dtype=outvec.dtype)
 
-    tf = np.abs(t)
+    t_f = np.abs(t)
 
     # For some reason numpy.sign has a different definition than Julia or MATLAB
     if isinstance(t, complex):
@@ -69,52 +69,52 @@ def expmv(t, A, v, tol=1e-7, krylov_dim=30):
     else:
         tsgn = np.sign(t)
 
-    tk = 0. * tf
+    t_k = 0. * t_f
     w = np.array(v, dtype=out_type)
     p = np.empty_like(w)
 
-    mx = m
-    while tk < tf:
-        tau = min(tf - tk, tau)
+    m_x = m
+    while t_k < t_f:
+        tau = min(t_f - t_k, tau)
         # Arnoldi procedure
-        vm[0] = w / beta
-        mx = m
+        v_m[0] = w / beta
+        m_x = m
 
         for j in range(m):
-            p = A.dot(vm[j])
+            p = A.dot(v_m[j])
 
-            hm[:j+1, j] = vm[:j+1, :].conj() @ p
-            tmp = hm[:j+1, j][:, np.newaxis] * vm[:j+1]
+            h_m[:j+1, j] = v_m[:j+1, :].conj() @ p
+            tmp = h_m[:j+1, j][:, np.newaxis] * v_m[:j+1]
             p -= np.sum(tmp, axis=0)
 
             s = norm(p)
             if s < btol: # happy-breakdown
-                tau = tf - tk
+                tau = t_f - t_k
                 err_loc = btol
 
-                F = expm(tsgn * tau * hm[:j+1, :j+1])
+                f_m = expm(tsgn * tau * h_m[:j+1, :j+1])
 
-                tmp = beta * F[:j+1, 0][:, np.newaxis] * vm[:j+1,:]
+                tmp = beta * f_m[:j+1, 0][:, np.newaxis] * v_m[:j+1, :]
                 w = np.sum(tmp, axis=0)
 
-                mx = j
+                m_x = j
                 break
 
-            hm[j+1, j] = s
-            vm[j+1] = p / s
+            h_m[j+1, j] = s
+            v_m[j+1] = p / s
 
-        hm[m + 1, m] = 1.
+        h_m[m + 1, m] = 1.
 
-        if mx == m:
-            avnorm = norm(A @ vm[m])
+        if m_x == m:
+            avnorm = norm(A @ v_m[m])
 
         # propagate using adaptive step size
-        it = 1
-        while it < maxiter and mx == m:
-            F = expm(tsgn * tau * hm)
+        i = 1
+        while i < maxiter and m_x == m:
+            f_m = expm(tsgn * tau * h_m)
 
-            err1 = abs(beta * F[m, 0])
-            err2 = abs(beta * F[m+1, 0] * avnorm)
+            err1 = abs(beta * f_m[m, 0])
+            err2 = abs(beta * f_m[m+1, 0] * avnorm)
 
             if err1 > 10*err2:	# err1 >> err2
                 err_loc = err2
@@ -128,23 +128,23 @@ def expmv(t, A, v, tol=1e-7, krylov_dim=30):
 
             # is time step sufficient?
             if err_loc <= delta * tau * (tau*tol/err_loc) ** r:
-                tmp = beta * F[:m+1, 0][:, np.newaxis] * vm[:m+1,:]
+                tmp = beta * f_m[:m+1, 0][:, np.newaxis] * v_m[:m+1, :]
                 w = np.sum(tmp, axis=0)
                 break
 
             # estimate new time-step
-            tau = gamma * tau * (tau * tol / err_loc) ** r 
-            it += 1
+            tau = gamma * tau * (tau * tol / err_loc) ** r
+            i += 1
 
-        if it == maxiter:
+        if i == maxiter:
             raise(RuntimeError("Number of iteration exceeded maxiter. "
                                "Requested tolerance might be too high."))
 
         beta = norm(w)
-        tk += tau
+        t_k += tau
         tau = gamma * tau * (tau * tol / err_loc) ** r # estimate new time-step
         err_loc = max(err_loc, rndoff)
-        
-        hm.fill(0.)
+
+        h_m.fill(0.)
 
     return w
